78673c9908dc27502dcee4a2a897341ff1d92b7f
Konstantinus Mesninus <haspadar@gmail.com>
2026-05-06 15:20:48 +0300

    #653: Surface phpcs.root_namespace via ComposerSettings decorator and migrate slevomat-style

--- src/Config/DefaultConfig.php
+++ src/Config/DefaultConfig.php
@@ -20,7 +20,7 @@ use Symfony\Component\Yaml\Yaml;
  *
  *     new DefaultConfig();
  *
- *     new DefaultConfig(paths: new ConfigPaths(composer: '/path/to/composer.json'));
+ *     new DefaultConfig(paths: new ConfigPaths(config: '/path/to/config.yaml'));
  */
 final readonly class DefaultConfig implements Config
 {
@@ -150,7 +150,6 @@ final readonly class DefaultConfig implements Config
             'infra.exclude' => $excludes,
             'jsonlint.patterns' => ['**/*.json', '**/*.json5', '**/*.jsonc'],
             'markdownlint.ignores' => (new TrailingGlobDirs($excludes))->toList(),
-            'phpcs.root_namespace' => (new ComposerRootNamespace($this->paths->composerJson()))->toString(),
             'infection.source.directories' => $projectIncludes,
             'typos.exclude' => (new TrailingSlashDirs($excludes))->toList(),
             'yamllint.ignore' => array_merge(
